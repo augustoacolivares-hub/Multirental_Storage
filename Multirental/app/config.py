fb5e527f0b3f88c9f03de80f49a04b0a73daf054
augustoacolivares-hub <augustoac.olivares@gmail.com>
2026-02-02 14:42:07 -0300

    Update Google Maps API key to placeholder
    Replaced the actual Google Maps API key with a placeholder.

--- Multirental/app/config.py
+++ Multirental/app/config.py
@@ -1,3 +1,4 @@
 class Config:
     SECRET_KEY = "claveultrasecreta"
-    GOOGLE_MAPS_API_KEY = "AIzaSyDP9el3po9LouLVf8gLbw0n0wwdNv0HU_E"  # Tu clave de API
+    GOOGLE_MAPS_API_KEY = "API KEY"  # Tu clave de API
+
